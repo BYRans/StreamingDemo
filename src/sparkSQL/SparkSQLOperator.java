d2c8c72abe07cd60c13d4286c1e6b06bd009a3e2
Ransford <dingyugege@sina.com>
2015-11-26 20:58:19 +0800

    SparkSQL Operator demo
    query hive table by HiveContext

--- src/sparkSQL/SparkSQLOperator.java
+++ src/sparkSQL/SparkSQLOperator.java
@@ -1,39 +1,27 @@
 package sparkSQL;
 
-import java.util.ArrayList;
-import java.util.List;
- 
+import java.io.IOException;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.api.java.JavaSQLContext;
-import org.apache.spark.sql.api.java.Row;
-import org.apache.spark.sql.hive.api.java.JavaHiveContext;
- 
- 
-/**
- * 注意：
- * 使用JavaHiveContext时
- * 1:需要在classpath下面增加三个配置文件：hive-site.xml,core-site.xml,hdfs-site.xml
- * 2:需要增加postgresql或mysql驱动包的依赖
- * 3:需要增加hive-jdbc,hive-exec的依赖
- *
- */
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.hive.HiveContext;
+
+/*park-submit --class sparkSQL.SparkSQLOperator --master  /home/dingyu/sparkSQL.jar*/
+
 public class SparkSQLOperator {
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("simpledemo").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaSQLContext sqlCtx = new JavaSQLContext(sc);
-        JavaHiveContext hiveCtx = new JavaHiveContext(sc);
-        testHive(hiveCtx);
-        sc.stop();
-        sc.close();
-    }
- 
-    //测试spark sql查询hive上面的表
-    public static void testHive(JavaHiveContext hiveCtx) {
-        List<Row> result = hiveCtx.sql("SELECT * from dy.sparkSQLTest").collect();
-        for (Row row : result) {
-            System.out.println(row.getString(0) + "," + row.getString(1));
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		SparkConf conf = new SparkConf();
+		conf.setAppName("SQL Executor");
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+		HiveContext sqlContext = new HiveContext(jsc.sc());
+		String finalSqlQueryStr = "select * from dy.sparksqltest d join xdf.sparksql x where x.xid = d.id";
+		Row[] results = sqlContext.sql(finalSqlQueryStr).collect();
+		for (Row row : results) {
+			System.out.println(row.getString(0) + "\t" + row.getString(1));
+		}
+		sqlContext.sql("create table dy.nice as "+finalSqlQueryStr);
+		jsc.stop();
+	}
+
 }
